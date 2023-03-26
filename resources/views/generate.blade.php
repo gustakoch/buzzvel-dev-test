886b2d0d137750621ffd8d5a022726290dcdee2f
Gustavo Koch <gustavok@outlook.com.br>
2023-03-26 20:02:07 -0300

    feat: :art: added placeholders to the inputs

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -11,7 +11,7 @@
             <div class="row mt-4">
                 <div class="col-sm-12 col-md-6">
                     <label for="name" class="form-label"><strong>Name:</strong></label>
-                    <input type="text" class="form-control" id="name" name="name" autocomplete="off" required>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Example: John" autocomplete="off" required>
                     <div class="invalid-feedback">
                         A name is required.
                     </div>
@@ -21,7 +21,7 @@
             <div class="row mt-3">
                 <div class="col-sm-12 col-md-6">
                     <label for="linkedin" class="form-label"><strong>LinkedIn:</strong></label>
-                    <input type="text" class="form-control" id="linkedin" name="linkedin" autocomplete="off" required>
+                    <input type="text" class="form-control" id="linkedin" name="linkedin" placeholder="Example URL: https://www.linkedin.com/in/john" autocomplete="off" required>
                     <div class="invalid-feedback">
                         A LinkedIn URL is required.
                     </div>
@@ -31,7 +31,7 @@
             <div class="row mt-3">
                 <div class="col-sm-12 col-md-6">
                     <label for="github" class="form-label"><strong>GitHub:</strong></label>
-                    <input type="text" class="form-control" id="github" name="github" autocomplete="off" required>
+                    <input type="text" class="form-control" id="github" name="github" placeholder="Example URL: https://github.com/john" autocomplete="off" required>
                     <div class="invalid-feedback">
                         A GitHub URL is required.
                     </div>
